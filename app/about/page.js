11151845d6b13c277e45ad0a345515a1bb95ba20
ujjwalnp <dhakalujjwal99@gmail.com>
2026-02-17 13:15:21 +0545

    fix: fix bg img size

--- app/about/page.js
+++ app/about/page.js
@@ -15,7 +15,7 @@ export default function AboutPage() {
       <Navbar />
 
       {/* Hero Section */}
-      <section className="relative h-[70vh] flex items-center justify-center overflow-hidden">
+      <section className="relative h-[60vh] flex items-center justify-center overflow-hidden">
         <div className="absolute inset-0">
           <Image
             src="/himalayan-mountain-range-panorama.jpg"
